202806546cf621edfb989cef18cd95761deb4fdb
3abdawy <elabd1996@gmail.com>
2024-10-30 02:53:06 +0300

    Add types for api service file

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,12 +1,25 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
 
-export const fetchTasks = userId =>
+interface Task {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
+}
+
+// Fetch tasks for a specific user
+export const fetchTasks = (userId: number): Promise<AxiosResponse<Task[]>> =>
   axios.get(`${API_URL}/todos?userId=${userId}`);
 
-export const updateTask = (id, completed) =>
+// Update the completion status of a task
+export const updateTask = (
+  id: number,
+  completed: boolean,
+): Promise<AxiosResponse<Task>> =>
   axios.put(`${API_URL}/todos/${id}`, {completed});
 
-export const addTask = title =>
+// Add a new task
+export const addTask = (title: string): Promise<AxiosResponse<Task>> =>
   axios.post(`${API_URL}/todos`, {title, completed: false});
